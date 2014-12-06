6660d3e5968ced5bd1a880568c14df3bf6fa34d2
Pawarsnehal23 <Pawarsnehal23@gmail.com>
2014-12-06 16:56:58 +0530

    Create HomePage.php

---
+++ HomePage.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+  <style>
+   body
+    {
+       margin:50px;
+    }
+     label
+     {
+             margin:30px;
+     }
+    label #message
+      {
+                 margin:10px;
+        }
+	</style>
+  </head>
+  <body>
+        <i style="color:green;font-size:1.2em;">
+            </br>1.Launch WebServer instance		
+		    </br>2.Launch your RDS instances and application server instances with script , the script will also create queue and topic . 
+			</br>3.Once application server and DB instances become ready , find the required details from
+			AWS management console OR shell script output which are required for "Configure all system details page" which will take all the provided information and store it to S3 for all further usage .There is no need of manually updating any of the code file.
+			</br>4.Note - Admin also need to launch worker instance via shell script in order to get this entire system working.
+			</br>5.Now, users can subscribe themselves.
+			</br>6.After subscribing users can try to upload pictures and then come here again to check their finished job.
+		</i>
+		
+	   <h3>Admin Section</h3> 
+	   <div style="border:1px solid black;width:600px;margin:30px;padding:30px;">
+	      <!--</br><a href="LaunchRDSInstance.php">Launch RDS instance</a>
+	      </br><a href="CreateQueue.php">Create Queue </a>
+		  </br><a href="CreateTopic.php">Create topic</a>-->
+		  </br><a href="ConfigureITM0544ImageProcessingSystem.php">Feed RDS , queue and topic information to system</a>
+	  </div>
+	   
+	    <h3>User Section</h3> 
+	    <div style="border:1px solid black;width:600px;margin:30px;padding:30px;">
+	      </br><a href="SubscribeToSNS.php">Subscribe To notification</a> 
+		  </br><a href="Gallary.php">View my jobs</a>
+	    </div>
+	   
+  </body>
+
+</html>  

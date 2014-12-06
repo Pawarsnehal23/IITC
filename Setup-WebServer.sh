d10ea93ca634573d420604ed6f77479849c1cfaa
Pawarsnehal23 <Pawarsnehal23@gmail.com>
2014-12-06 18:21:07 +0530

    Update Setup-WebServer.sh

--- Setup-WebServer.sh
+++ Setup-WebServer.sh
@@ -6,7 +6,10 @@
    
    sudo apt-get -y update 
    sudo apt-get -y install --force-yes apache2 wget php5 php5-curl curl git php5-mysql  wget
-
+    
+   #Install Git	
+   sudo apt-get install git
+   
    #enable mysqli
    sudo sed  -i  '/;mysqli.allow_local_infile = On/c\mysqli.allow_local_infile = On' /etc/php5/apache2/php.ini
    #restart apache
@@ -15,16 +18,16 @@
    #chnage permissions
    sudo chmod -R 777 /var/www/html 
   
-   # Get files from web server 
-   wget http://ec2-54-148-142-162.us-west-2.compute.amazonaws.com/composer.json
+   #Get required files
+   git clone https://github.com/Pawarsnehal23/IITC.git
+   
+   mv /IITC/composer.json composer.json
    
    # Get composer
    curl -sS https://getcomposer.org/installer | sudo php
    sudo php composer.phar install
    
-   #Get required files
-   git clone https://github.com/Pawarsnehal23/IITC.git
-   
+     
    # Move file to www 
    mv /IITC/ConfigureDetails.php /var/www/html
    mv /IITC/ConfigureITM0544ImageProcessingSystem.php /var/www/html

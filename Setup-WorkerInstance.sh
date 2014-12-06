7bc9ef26a1c6270ae6edabc253aa58b19ff29895
Pawarsnehal23 <Pawarsnehal23@gmail.com>
2014-12-07 02:59:18 +0530

    Update Setup-WorkerInstance.sh

--- Setup-WorkerInstance.sh
+++ Setup-WorkerInstance.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
   
-   sudo apt-get -y update 
-   sudo apt-get -y install --force-yes apache2 wget php5 php5-curl curl git php5-mysql  wget
+   sudo apt-get -y update ;
+   sudo apt-get -y install --force-yes apache2 wget php5 php5-curl curl git php5-mysql  wget;
    
    #Install Git	
-   sudo apt-get install git 
+   sudo apt-get install git ;
    
    #Install GD 
-   sudo apt-get install php5-gd wget
+   sudo apt-get install php5-gd wget;
    
    sleep 10;
    
    #Enable Mysqli
-   sudo sed  -i  '/;mysqli.allow_local_infile = On/c\mysqli.allow_local_infile = On' /etc/php5/apache2/php.ini
-   sudo /etc/init.d/apache2 restart
+   sudo sed  -i  '/;mysqli.allow_local_infile = On/c\mysqli.allow_local_infile = On' /etc/php5/apache2/php.ini;
+   
+   sudo /etc/init.d/apache2 restart;
    
    sleep 10;
    
    #Install font required for creating thumbnail
-   sudo apt-get install msttcorefonts
+   sudo apt-get install msttcorefonts;
    
    #chnage permissions
-   sudo chmod -R 777 /var/www/html 
+   sudo chmod -R 777 /var/www/html ;
    
    #Get Code files from Github account
-   git clone https://github.com/Pawarsnehal23/IITC.git
+   git clone https://github.com/Pawarsnehal23/IITC.git;
    
-   mv /IITC/composer.json /composer.json
+   mv /IITC/composer.json /composer.json;
   
    # Get composer
    curl -sS https://getcomposer.org/installer | sudo php

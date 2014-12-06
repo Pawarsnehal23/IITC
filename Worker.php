6a8982ca2e6d66cae4b3dbcac710331e021c41d0
Pawarsnehal23 <Pawarsnehal23@gmail.com>
2014-12-07 04:18:03 +0530

    Update Worker.php

--- Worker.php
+++ Worker.php
@@ -1,42 +1,42 @@
 #!/bin/bash
   
-   sudo apt-get -y update ;
-   sudo apt-get -y install --force-yes apache2 wget php5 php5-curl curl git php5-mysql  wget;
+   #Install Zip
+   sudo apt-get install zip
+   sudo apt-get install unzip
    
-   #Install Git	
-   sudo apt-get -y install --force-yes git ;
+   sudo apt-get -y update 
+   sudo apt-get -y install --force-yes apache2 wget php5 php5-curl curl git php5-mysql  wget
    
    #Install GD 
-   sudo apt-get -y install --force-yes php5-gd ;
-   
-   sleep 10;
-   
-   #Enable Mysqli
-   sudo sed  -i  '/;mysqli.allow_local_infile = On/c\mysqli.allow_local_infile = On' /etc/php5/apache2/php.ini;
-   
-   sudo /etc/init.d/apache2 restart;
-   
-   sleep 10;
+   sudo apt-get install php5-gd
    
    #Install font required for creating thumbnail
-   sudo apt-get -y install --force-yes msttcorefonts;
+   sudo apt-get install msttcorefonts
+   
+   #Install Git	
+   sudo apt-get install git
+   
+   #Enable Mysqli
+   sudo sed  -i  '/;mysqli.allow_local_infile = On/c\mysqli.allow_local_infile = On' /etc/php5/apache2/php.ini
+   sudo /etc/init.d/apache2 restart
    
    #chnage permissions
-   sudo chmod -R 777 /var/www/html ;
-   
-   #Get Code files from Github account
-   git clone https://github.com/Pawarsnehal23/IITC.git;
-   
-   mv /IITC/composer.json /composer.json;
+   sudo chmod -R 777 /var/www/html 
   
+   # Get files from web server to application server
+   wget http://ec2-54-148-142-162.us-west-2.compute.amazonaws.com/composer.json
+   
    # Get composer
    curl -sS https://getcomposer.org/installer | sudo php
    sudo php composer.phar install
    
-        
+   wget http://ec2-54-148-142-162.us-west-2.compute.amazonaws.com/WorkerPhp.zip
+
+   unzip WorkerPhp.zip 
+     
    # Move file to www 
    mv /vendor /var/www/html
-   mv /IITC/Worker.php /var/www/html
+   mv /Worker.php /var/www/html
    
    #Launch worker
    php /var/www/html/Worker.php &

18284a4295c1661dfd8333b93bebe4557abf0817
Pawarsnehal23 <Pawarsnehal23@gmail.com>
2014-12-07 12:51:42 +0530

    Update Worker.php

--- Worker.php
+++ Worker.php
@@ -8,32 +8,36 @@
    sudo apt-get -y install --force-yes apache2 wget php5 php5-curl curl git php5-mysql 
    
    #Install GD 
-   sudo apt-get install php5-gd
+   sudo apt-get install php5-gd && sudo service apache2 restart
    
    #Install font required for creating thumbnail
    sudo apt-get install msttcorefonts
    
    #Enable Mysqli
    sudo sed  -i  '/;mysqli.allow_local_infile = On/c\mysqli.allow_local_infile = On' /etc/php5/apache2/php.ini
+   sudo sed  -i  '/;extension=php_gd2.dll/c\extension=php_gd2.dll' /etc/php5/apache2/php.ini
+   
    sudo /etc/init.d/apache2 restart
    
    #chnage permissions
    sudo chmod -R 777 /var/www/html 
-  
-   # Get files from web server to application server
-   wget http://ec2-54-148-142-162.us-west-2.compute.amazonaws.com/composer.json
    
+   #chnage permissions
+   sudo chmod -R 777 /var/www/html ;
+   
+   #Get Code files from Github account
+   git clone https://github.com/Pawarsnehal23/IITC.git;
+   
+   mv /IITC/composer.json /composer.json;
+  
    # Get composer
    curl -sS https://getcomposer.org/installer | sudo php
    sudo php composer.phar install
    
-   wget http://ec2-54-148-142-162.us-west-2.compute.amazonaws.com/WorkerPhp.zip
-
-   unzip WorkerPhp.zip 
-     
+        
    # Move file to www 
    mv /vendor /var/www/html
-   mv /Worker.php /var/www/html
+   mv /IITC/Worker.php /var/www/html
    
    #Launch worker
    php /var/www/html/Worker.php &

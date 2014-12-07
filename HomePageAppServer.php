122dfc3e1632ee96c09b06c947f7d0af724debb8
Pawarsnehal23 <Pawarsnehal23@gmail.com>
2014-12-07 16:34:07 +0530

    Create HomePageAppServer.php

---
+++ HomePageAppServer.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+  <style>
+   body
+    {
+       margin:50px;
+    }
+     label
+     {
+             margin:30px;
+     }
+    label #message
+      {
+                 margin:10px;
+        }
+	</style>
+  </head>
+  <body>
+        <i style="color:green;font-size:1.2em;">
+            </br>1.If your email address has been subscribed by admin , then you will get email notification once your job completes
+			</br>2.After subscribing users can try to upload pictures and then come here again to check their finished job.
+		</i>
+		
+	    <h3>User Section</h3> 
+	    <div style="border:1px solid black;width:600px;margin:30px;padding:30px;">
+	      </br><a href="Index.php">Upload New Images</a> 
+		  </br><a href="Gallary.php">View my jobs</a>
+	    </div>
+	   
+  </body>
+
+</html>  

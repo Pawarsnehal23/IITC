01875f7848ab2ce3d8937ff1c1326ccfc170ba62
Pawarsnehal23 <Pawarsnehal23@gmail.com>
2014-12-07 13:06:11 +0530

    Update Setup-WorkerInstance.sh

--- Setup-WorkerInstance.sh
+++ Setup-WorkerInstance.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
   
-   sudo apt-get -y update ;
-   sudo apt-get -y install --force-yes apache2 wget php5 php5-curl curl git php5-mysql  wget;
-   
-   #Install Git	
-   sudo apt-get -y install --force-yes git ;
-   
-   #Install GD 
-   sudo aptitude -y install --force-yes php5-gd ;
-   
-   sleep 10;
-   
-   #Enable Mysqli
-   sudo sed  -i  '/;mysqli.allow_local_infile = On/c\mysqli.allow_local_infile = On' /etc/php5/apache2/php.ini;
-   
-   sudo /etc/init.d/apache2 restart;
-   
-   sleep 10;
+   sudo apt-get -y update 
+   sudo apt-get -y install --force-yes apache2 wget php5 php5-curl curl git php5-mysql php5-gd
    
    #Install font required for creating thumbnail
-   sudo apt-get -y install --force-yes msttcorefonts;
+   sudo apt-get -y install msttcorefonts
+   
+   #Enable Mysqli
+   sudo sed  -i  '/;mysqli.allow_local_infile = On/c\mysqli.allow_local_infile = On' /etc/php5/apache2/php.ini
+   sudo sed  -i  '/;extension=php_gd2.dll/c\extension=php_gd2.dll' /etc/php5/apache2/php.ini
+   
+   sudo /etc/init.d/apache2 restart
    
    #chnage permissions
-   sudo chmod -R 777 /var/www/html ;
-   
+   sudo chmod -R 777 /var/www/html 
+     
    #Get Code files from Github account
    git clone https://github.com/Pawarsnehal23/IITC.git;
    
